bd7a2319f3214b82e8a473024fbf552263e54ddb
Sahil singh <sahilsingh.afl@gamil.com>
2026-03-01 19:23:19 +0530

    Creating JWT token

---
+++ Router/chatRouter.js
@@ -0,0 +1,53 @@
+import express from 'express';
+import SchemaData from '../UserSchema/userSchema.js';
+import { generateToken } from '../Jwt/token.js';
+
+const router = express.Router();
+
+router.get('/data', (req, res) => {
+    res.send("This is the router");
+})
+
+router.post('/signup', async (req, res) => {
+    try {
+        const userSchemaData = new SchemaData(req.body);
+        const saveData = await userSchemaData.save();
+
+        res.status(201).json({ message: "Signup successful", data: saveData })
+    } catch (err) {
+        if (err.code === 11000) {
+            return res.status(400).json({
+                message: "Email already exists"
+            });
+        }
+
+        res.status(500).json({
+            message: "Server error"
+        });
+    }
+})
+
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const matchData = await SchemaData.findOne({ email });
+        if (!matchData) {
+            return res.status(401).json({ message: "Invalid Email" });
+        }
+
+        if (matchData.password !== password) {
+            return res.status(401).json({ message: "Invalid Password" });
+        }
+
+        const payload = {
+            id: matchData.id,
+            name : matchData.name
+        }
+        const token = generateToken(payload);
+        res.status(201).json({ message: "Login successful", data: matchData, token: token })
+    } catch (err) {
+        console.log(err)
+        return res.status(500).json({ err })
+    }
+})
+export default router;
--- chat-backend.js
+++ chat-backend.js
@@ -1,9 +1,29 @@
-import express from 'express'
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import chatRouter from './Router/chatRouter.js'
+import mongoose from 'mongoose';
 
+dotenv.config()
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use('/user', chatRouter);
+
+const mongoUrl = process.env.MONGO_URL;
+await mongoose.connect(mongoUrl)
+    .then(()=>{
+        console.log("Mongodb has connected")
+    })
 
 app.get('/', (req,res) => {
     res.send("Hello Backend")
 })
-const PORT = process.env.PORT
-app.listen(PORT,console.log("Connected"));
+
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+    console.log("Connected");
+});
